cdc3d738bac2a01a47c422fbab46d387f2e77026
Danz <DvRossen@outlook.com>
2023-06-27 02:25:11 +0200

    how to page working

--- src/screens/explainPage.js
+++ src/screens/explainPage.js
@@ -4,11 +4,13 @@ export function ExplainPage({ setState }) {
   console.log("explain");
   return (
     <div className="Container">
-      <button className="bck-btn" onClick={() => setState(0)}>
-        {`<`} Back
-      </button>
-      <div className="TitleCard">
-        <h1 className="Title">Other page</h1>
+      <div className="display-field">
+        <button className="bck-btn" onClick={() => setState(0)}>
+          {`<`} Back
+        </button>
+        <div className="Card">
+          <h1>Other page</h1>
+        </div>
       </div>
     </div>
   );
